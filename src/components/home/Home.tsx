600df983f41f06e193f63ee4d5040283a4f18dde
SANG LE <sqle@ualberta.ca>
2021-10-20 21:28:19 -0600

    fix responsiveness

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,9 +1,10 @@
-import React, { useCallback, FC } from "react";
+import React, { useCallback, FC, useState } from "react";
 import CreatePost, { PostRequest } from "./CreatePost";
 import Post from "./Post";
 
 const Home: FC = () => {
-  // const [title, setTitle] = useState("");
+  const [title, setTitle] = useState("");
+  const [isPost, setIsPost] = useState(false);
 
   // const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
   //   setTitle(e.target.value);
@@ -12,8 +13,8 @@ const Home: FC = () => {
     console.log(postRequest.text);
   }, []);
 
-  const title =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore";
+  // const title =
+  //   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore";
 
   return (
     <div className="container">
