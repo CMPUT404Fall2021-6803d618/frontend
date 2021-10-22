42beb18a7c909dff3d18992d95c7e27f21ce098d
Trung <quoctrun@ualberta.ca>
2021-10-22 17:01:38 -0600

    add funtionaility to create a tweet

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,25 +1,33 @@
-import React, { useCallback, FC } from "react";
+import React, { useState, useCallback, FC } from "react";
 import CreatePost, { PostRequest } from "./CreatePost";
 import Post from "./Post";
 
 const Home: FC = () => {
   // const [title, setTitle] = useState("");
+  const [posts, setPosts] = useState<PostRequest[]>([]);
+  const [title, setTitle] = useState("");
 
   // const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
   //   setTitle(e.target.value);
   // }
-  const addPost = useCallback((postRequest: PostRequest) => {
-    console.log(postRequest.text);
-  }, []);
+  const addPost = useCallback(
+    (postRequest: PostRequest) => {
+      setTitle(postRequest.text);
+      setPosts([...posts, postRequest]);
+    },
+    [posts]
+  );
 
-  const title =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore";
+  // const title =
+  // ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore");
 
   return (
     <div className="container">
       <CreatePost onAdd={addPost} />
-      <Post isMedia={true} title={title} />
-      <Post isMedia={false} title={title} />
+      {posts.map((post, index) => (
+        <Post key={index} isMedia={false} title={post.text} />
+      ))}
+      {/* {posts && <Post isMedia={false} title={title} />} */}
     </div>
   );
 };
