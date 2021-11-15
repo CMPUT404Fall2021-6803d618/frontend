6aac8b536b5068e75e3e202d15e6fb841313ea8d
Khang Vuong <khangdieuvuong@gmail.com>
2021-11-14 21:52:57 -0700

    paginate inbox

---
+++ src/services/BaseService.ts
@@ -0,0 +1,55 @@
+import { axios } from "utils/axios";
+
+interface IBaseService<T> {
+  getAll: (url: string, dataKey?: string) => Promise<T[]>;
+  getPaginate: (url: string, page: number, size: number, dataKey?: string) => Promise<PaginateResponse<T>>;
+}
+
+interface PaginateResponse<T> {
+  page: number;
+  size: number;
+  count: number;
+  items: T[];
+}
+
+export class BaseService<T> implements IBaseService<T> {
+  private DEFAULT_PAGE = 1;
+  private DEFAULT_SIZE = 5;
+
+  public async getAll(url: string, dataKey = "items"): Promise<T[]> {
+    const res = await this.getPaginate(url, this.DEFAULT_PAGE, this.DEFAULT_SIZE, dataKey);
+    const { page, size, count, items } = res;
+    if (size !== undefined && count !== undefined && page !== undefined) {
+      if (count <= size) {
+        return items;
+      } else {
+        const remainingCount = count - items.length;
+        const pageCount = Math.ceil(remainingCount / size);
+        const pages = Array.from(Array(pageCount).keys()).map((i) => i + 2);
+        const remainingItems = await Promise.all(
+          pages.map(async (p) => (await this.getPaginate(url, p, size, dataKey)).items)
+        );
+        return [items, ...remainingItems].flat();
+      }
+    } else {
+      return items;
+    }
+  }
+
+  public async getPaginate(url: string, page: number, size: number, dataKey = "items"): Promise<PaginateResponse<T>> {
+    const { data } = await axios.get(url, {
+      params: {
+        page,
+        size,
+      },
+    });
+    const items: T[] = data[dataKey] ?? [];
+
+    return {
+      page,
+      size,
+      count: data.count,
+      items,
+    };
+  }
+}
