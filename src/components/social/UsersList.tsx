6c7d0130afd3e63f09916cc3052a14939eab2806
lucas <lucaszeng99@gmail.com>
2021-11-21 18:36:34 -0700

    fix typo

--- src/components/social/UsersList.tsx
+++ src/components/social/UsersList.tsx
@@ -1,7 +1,7 @@
 import React, { FC, MouseEvent, useCallback } from "react";
 import Stack from "@mui/material/Stack";
 
-const UsersList: TC = ({ children }) => (
+const UsersList: FC = ({ children }) => (
   <Stack spacing={1} sx={{ margin: 1 }}>
     {children}
   </Stack>
