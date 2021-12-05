cd51fa4b3ea354c7fecb8135a05cc653f91e1cde
Khang Vuong <khangdieuvuong@gmail.com>
2021-12-04 18:52:23 -0700

    fix component import 404

--- tests/NotFound.test.tsx
+++ tests/NotFound.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import NotFound from "../components/404/NotFound";
+import NotFound from "../src/components/404/NotFound";
 
 test("renders learn react link", () => {
   render(<NotFound />);
